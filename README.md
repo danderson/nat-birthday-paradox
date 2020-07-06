7e44ef99e2102382389915a9f207b221fd437123
David Anderson <dave@natulte.net>
2020-07-06 23:23:57 +0000

    Rewrite paradox simulator to just use the math straight up.

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # nat-birthday-paradox
-A small simulator that illustrates the birthday paradox as applied to NAT traversal.
+
+A small simulator that illustrates the birthday paradox as applied to
+NAT traversal. Run `paradox.py --help` for quick instructions.
+
+## Easy birthday attack
+
+In the easy case, one peer is behind an NAT with Endpoint-Depdendent
+Mapping (EDM, which varies its WAN source port for each destination),
+while the other is behind an NAT with Endpoint-Independent Mapping
+(EIM, which uses a single WAN source port for all traffic to a
+particular LAN socket).
+
+To traverse this, the EDM peer creates N sockets, and probes the one
+ip:port of the EIM peer. The EIM peer creates a single socket, and
+uses it to probe M random ports on the EDM NAT.
+
+`./paradox.py --a-side-probes=256 --b-side-probes=256` shows that with
+256 sockets on the EDM side and 256 random probes from the EIM side,
+there's a 64% probability of a collision (meaning the peers can
+communicate). This is slightly better than the 50% the straight
+birthday paradox predicts, because we're looking for collisions
+between two sets, rather than collisions within a single set.
+
+## Hard birthday attack
+
+In the hard case, both peers are behind an EDM NAT. The peers create N
+and M sockets each, and probe NP, MP random ports on the other
+NAT. Each probe results in a new mapping on their NAT, and in firewall
+state that requires an exact match of both source and destination port
+in order to pass traffic.
+
+In other words, the peers probe `N*NP` and `M*MP` combinations of
+`(wan_src_port, wan_remote_port)` each, which doubles the number of
+bits in the search space from `2^16` in the easy case to `2^(16+16)`
+in the hard case.
+
+`./paradox.py --hard --a-side-probes=256 --b-side-probes=256` shows
+that the same amount of packets results in a ~0% chance of
+connectivity. You have to crank both sides to 65536 probes each
+(e.g. 256 probes from each of 256 sockets, on both sides) to get back
+to a 64% probability of communication.
--- paradox.py
+++ paradox.py
@@ -1,76 +1,105 @@
 #!/usr/bin/env python
-#
-# This file implements the birthday paradox attack between two NATs
-# with endpoint-dependent mapping and endpoint-independent
-# firewalling.
 
+import argparse
 import random
 random.seed()
 
-# Each round, both sides generate a random secret of their own (the
-# mapped port on their NAT), and try to guess the other side's secret
-# (the mapped port on the remote NAT). Success is when either one
-# guesses any of the other side's N previous secrets.
+def ports():
+    return range(1025, 65536)
 
-class Clock(object):
-    def __init__(self):
-        self.time = 0
+def easy_hard(hard_side_sockets=256, easy_side_probes=256):
+    # On the hard side, create N sockets and send probes to the one
+    # (known, correct) ip:port on the easy side. The net effect, for
+    # the purposes of this simulation, is that we'll have N port
+    # mappings on the hard NAT, all wanting to see traffic from the
+    # easy ip:port.
+    hard_side_ports = set(random.sample(ports(), k=hard_side_sockets))
 
-    def advance(self, delta):
-        self.time += delta
+    # On the easy side, create a single socket and probe M ports on
+    # the hard side.
+    easy_side_probes = set(random.sample(ports(), k=easy_side_probes))
 
-    def now(self):
-        return self.time
+    hits = easy_side_probes.intersection(hard_side_ports)
+    return len(hits) > 0
 
-class Device(object):
-    def __init__(self, clock, valid_window):
-        self.clock = clock
-        self.valid_window = valid_window
-        self.mapped = {}
+def hard_hard(a_probes=256, b_probes=256):
+    # On each hard side, probe N targets. Each one results in a random
+    # *pair* of ports: the NAT-allocated port, and the target port
+    # from which packets must return if they're to cross the firewall.
+    a_ports = random.sample(ports(), k=a_probes)
+    a_probes = random.sample(ports(), k=a_probes)
+    a_state = set(zip(a_ports, a_probes))
 
-    def guess(self, other):
-        """Try to guess a working port on other.
+    b_ports = random.sample(ports(), k=b_probes)
+    b_probes = random.sample(ports(), k=b_probes)
+    # Note: probes/ports is flipped compared to a_state, so that we
+    # can intersect the two sets and find matches.
+    b_state = set(zip(b_probes, b_ports))
 
-        Returns True if we guessed correctly (meaning a packet was
-        delivered), False otherwise.
+    # Traversal only succeeds if the two sides independently guess the
+    # same ports (albeit mirrored - A's outbound is B's inbound and
+    # vice-versa).
+    hits = a_state.intersection(b_state)
+    return len(hits) > 0
 
-        As a side-effect, this also opens a new port on self, which
-        others could use to reach self.
-        """
-        myPort = random.randrange(1025, 65535)
-        self.mapped[myPort] = self.clock.now()
+def probability(a_side=256, b_side=256, hard=False):
+    # The math is not quite the same as the classic birthday paradox,
+    # because we're looking for collisions between two different sets,
+    # rather than collisions within one set. The net result is that
+    # the probability curve skews slightly more in our favor, e.g. we
+    # get 66% likelihood of success where the straight birthday
+    # paradox would get 50%.
+    #
+    # We do the math on this the somewhat naive way, which means we'll
+    # probably accumulate rounding errors even with double precision
+    # floats. Since this is just an approximate simulation, that's
+    # okay. We could always use higher precision decimals if we wanted
+    # to.
+    num_options = len(ports())
+    if hard:
+        # In hard mode, the search space doubles, because we now have
+        # to find collisions on _pairs_ of ports, each of which can
+        # cover the entire port range.
+        num_options *= num_options
+    p = 1.0
+    for i in range(b_side):
+        p *= (num_options-a_side-i)/(num_options-i)
+    return 1.0 - p
 
-        theirPort = random.randrange(1025, 65535)
-        hit = other.mapped.get(theirPort)
-        if not hit or self.clock.now()-hit > other.valid_window:
-            return False
-        return True
+def main():
+    p = argparse.ArgumentParser(description="Calculate birthday paradox probabilities for NAT traversal")
+    p.add_argument("--hard", action="store_true", help="Simulate two endpoint-dependent NATs instead of one")
+    p.add_argument("--a-side-probes", type=int, default=256, help="number of probe attempts from the A side")
+    p.add_argument("--b-side-probes", type=int, default=256, help="number of probe attempts from the B side")
+    args = p.parse_args()
+    prob = probability(args.a_side_probes, args.b_side_probes, args.hard)
+    num_ports = len(ports())
+    if args.hard:
+        pcta = args.a_side_probes/(num_ports*num_ports)*100
+        pctb = args.b_side_probes/(num_ports*num_ports)*100
+        print("""
+Simulating the hard case:
+  peer A -> endpoint-dependent NAT --- endpoint-dependent NAT <- peer B
 
-def attempt(valid_window_secs=10, xmit_delta=0.1):
-    clock = Clock()
-    a, b = Device(clock, valid_window_secs), Device(clock, valid_window_secs)
+  A probes {} combinations of (source port, target port)
+    (this is {:.2g}% of the search space)
+  B probes {} combinations of (source port, target port)
+    (this is {:.2g}% of the search space)
+""".format(args.a_side_probes, pcta, args.b_side_probes, pctb))
+    else:
+        pcta = args.a_side_probes/num_ports*100
+        pctb = args.b_side_probes/num_ports*100
+        print("""
+Simulating the easy case:
 
-    i=1
-    while not (a.guess(b) or b.guess(a)):
-        i+=1
-        clock.advance(xmit_delta)
-    return i, clock.now()
+  peer A -> endpoint-dependent NAT --- endpoint-independent NAT <- peer B
 
-attempts = 1000
-window_secs = 10
-delta = 0.1
+  A probes 1 target port on B from {} source ports
+    (this is {:.2g}% of the search space)
+  B probes {} target ports on A from 1 source port
+    (this is {:.2g}% of the search space)
+""".format(args.a_side_probes, pcta, args.b_side_probes, pctb))
+    print("Probability of successful traversal: %.2f%%" % (prob*100))
 
-results = [attempt() for _ in range(attempts)]
-times = [x[1] for x in results]
-min_time, max_time = int(min(times)), int(max(times))
-avg_time = sum(times)/len(times)
-median_time = sorted(times)[len(times)//2]
-
-buckets = [0 for _ in range((max_time//5)+1)]
-for t in times:
-    buckets[int(t)//5] += 1
-cum = [sum(buckets[:i+1]) for i in range(len(buckets))]
-
-for i in range(len(buckets)):
-    print("{},{},{},{}".format(i*5, buckets[i], cum[i], cum[i]/attempts))
-#print("min={}s max={}s avg={}s median={}s".format(min_time, max_time, avg_time, median_time))
+if __name__ == "__main__":
+    main()
